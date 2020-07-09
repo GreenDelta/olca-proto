6387ab73426351f2244cf66b05c4cb73d833e0e7
msrocka <michael.srocka@gmail.com>
2020-07-09 17:21:51 +0200

    need to check the go deps

--- go/example.go
+++ go/example.go
@@ -1,8 +1,13 @@
 package main
 
 import (
-	_ "main/protolca"
+	lca "main/protolca"
 )
 
 func main() {
+	flow := lca.Flow{
+		Type:     "Flow",
+		Name:     "Steel",
+		FlowType: lca.FlowType_PRODUCT_FLOW,
+	}
 }
