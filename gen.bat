e09f9b98d01b7db97d296da9f78ccd93f684a704
msrocka <michael.srocka@gmail.com>
2020-10-16 13:57:36 +0200

    https://github.com/grpc/grpc-java
    add these dependencies:
    
    ```xml
    <dependency>
      <groupId>io.grpc</groupId>
      <artifactId>grpc-netty-shaded</artifactId>
      <version>1.32.2</version>
    </dependency>
    <dependency>
      <groupId>io.grpc</groupId>
      <artifactId>grpc-protobuf</artifactId>
      <version>1.32.2</version>
    </dependency>
    <dependency>
      <groupId>io.grpc</groupId>
      <artifactId>grpc-stub</artifactId>
      <version>1.32.2</version>
    </dependency>
    <dependency>
      <groupId>org.apache.tomcat</groupId>
      <artifactId>annotations-api</artifactId>
      <version>6.0.53</version>
    </dependency>
    ```

--- gen.bat
+++ gen.bat
@@ -1,14 +1,19 @@
 @echo off
 
-rem Java
+
+rem Generate the Java model
 protoc olca.proto --java_out=src\main\java
+protoc services.proto --java_out=src\main\java
+rem Generate the gRPC services
+protoc --plugin=protoc-gen-grpc-java.exe --grpc-java_out=src\main\java --proto_path=. services.proto
+
 
 rem Python
-protoc olca.proto --python_out=python/protolca --mypy_out=python/protolca
-xcopy /y python\protolca\olca_pb2.py python\protolca\__init__.py
-xcopy /y python\protolca\olca_pb2.pyi python\protolca\__init__.pyi
-del python\protolca\olca_pb2.py
-del python\protolca\olca_pb2.pyi
+rem protoc olca.proto --python_out=python/protolca --mypy_out=python/protolca
+rem xcopy /y python\protolca\olca_pb2.py python\protolca\__init__.py
+rem xcopy /y python\protolca\olca_pb2.pyi python\protolca\__init__.pyi
+rem del python\protolca\olca_pb2.py
+rem del python\protolca\olca_pb2.pyi
 
 rem Go
-protoc olca.proto --go_out=go\protolca
+rem protoc olca.proto --go_out=go\protolca
