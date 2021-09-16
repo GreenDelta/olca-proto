59f71b7aad97d5d10b49383076a5a8702cda5c0d
msrocka <michael.srocka@gmail.com>
2021-09-16 13:28:14 +0200

    updated protos; generate proto-lib in sub-module

---
+++ olca-proto/src/test/java/proto/FlowTest.java
@@ -0,0 +1,13 @@
+package proto;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class FlowTest {
+
+  @Test
+  public void test() {
+	  System.out.println("running a test ...");
+	  Assert.assertTrue(true);
+  }
+}
